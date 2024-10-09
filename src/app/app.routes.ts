0218d2fae7561c19beb0456382479f0747b6ab5c
Ashutosh <ashutosh.ak.3216@gmail.com>
2024-10-09 15:26:48 +0530

    fixed : whatsapp validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import {VirtualOfficePageComponent} from "./virtual-office-page/virtual-office-page.component";
+// import {CareerComponent} from "./career/career.component";
+// import {PrivacyPolicyComponent} from "./privacy-policy/privacy-policy.component";
 // import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
   // { path: '', component: HomeComponent },
   { path: 'virtual-office', redirectTo: 'virtual-office/', pathMatch: 'full' },
   { path: 'virtual-office/', component: VirtualOfficePageComponent },
+  // { path: 'WeCareAboutPrivacy', redirectTo: 'WeCareAboutPrivacy/', pathMatch: 'full' },
+  // { path: 'WeCareAboutPrivacy/', component: PrivacyPolicyComponent },
+  // { path: 'careers', redirectTo: 'careers/', pathMatch: 'full' },
+  // { path: 'careers/', component: CareerComponent },
 
 ];
