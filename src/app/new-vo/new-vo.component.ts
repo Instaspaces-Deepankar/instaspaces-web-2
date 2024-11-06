0a78e847920fbd2794a7df85b182e5d1676705d1
Deepankar <deepankar.instaspaces@gmail.com>
2024-11-06 11:54:50 +0530

    created component for new vo

---
+++ src/app/new-vo/new-vo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-vo',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-vo.component.html',
+  styleUrl: './new-vo.component.scss'
+})
+export class NewVoComponent {
+
+}
---
+++ src/app/new-vo/vo-hero/vo-hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vo-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './vo-hero.component.html',
+  styleUrl: './vo-hero.component.scss'
+})
+export class VoHeroComponent {
+
+}
