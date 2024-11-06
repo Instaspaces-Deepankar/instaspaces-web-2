a9a3082aa8f005233c7ec82a3a158975d1f824dd
deepesh chourasia <deepeshch.instaspaces@gmail.com>
2024-11-06 17:32:50 +0530

    created vo hero section and header

--- src/app/new-vo/new-vo.component.ts
+++ src/app/new-vo/new-vo.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { VoHeroComponent } from "./vo-hero/vo-hero.component";
 
 @Component({
   selector: 'app-new-vo',
   standalone: true,
-  imports: [],
+  imports: [VoHeroComponent],
   templateUrl: './new-vo.component.html',
   styleUrl: './new-vo.component.scss'
 })
--- src/app/new-vo/vo-hero/vo-hero.component.ts
+++ src/app/new-vo/vo-hero/vo-hero.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './vo-hero.component.html',
-  styleUrl: './vo-hero.component.scss'
+  styleUrls: ['./vo-hero.component.scss']
 })
-export class VoHeroComponent {
-
-}
+export class VoHeroComponent {}
