f6252c7b8d99be08f558717ba87f7d1de6f2e672
Instaspaces-Deepankar <deepankar.instaspaces@gmail.com>
2024-12-16 12:03:05 +0530

    `Added new components and updated existing ones in the New VO section`

--- src/app/new-vo/happy-customer/happy-customer.component.ts
+++ src/app/new-vo/happy-customer/happy-customer.component.ts
@@ -12,59 +12,75 @@ import { CommonModule } from '@angular/common';
 })
 export class HappyCustomerComponent {
   currentIndex: number = 0;
-  totalItems: number = 6;
-  itemsToShow: number = 3;
+  totalItems: number = 8;
+  itemsToShow: number = 8;
 
   faChevronLeft = faChevronLeft;
   faChevronRight = faChevronRight;
 
   customers = [
     {
-      name: 'Customer 1',
+      name: 'Ed Tech',
       images: [
         'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png'
+        'https://isicons.gumlet.io/logo_wall/upgrad.png',
+        'https://isicons.gumlet.io/logo_wall/Educational-Excellencia.png'
       ]
     },
     {
-      name: 'Customer 2',
+      name: 'CA Firm',
       images: [
+        'https://isicons.gumlet.io/logo_wall/CA-Shyam-Goel.png',
+        'https://isicons.gumlet.io/logo_wall/Clear-tax.png',
+        'https://isicons.gumlet.io/logo_wall/Vakil-Search.png'
+      ]
+    },
+    {
+      name: 'Finance',
+      images: [
+        'https://isicons.gumlet.io/logo_wall/pinelabs.png',
+        'https://isicons.gumlet.io/logo_wall/hp.png',
+        'https://isicons.gumlet.io/logo_wall/Clix-Capita.png'
+      ]
+    },
+    {
+      name: 'Entertainment',
+      images: [
+        'https://isicons.gumlet.io/logo_wall/Book-My-show.png',
+        'https://isicons.gumlet.io/logo_wall/dharma.png',
+        'https://isicons.gumlet.io/logo_wall/Exotel.png'
+      ]
+    },
+    {
+      name: 'Event Management',
+      images: [
+        'https://isicons.gumlet.io/logo_wall/GIG-Hospitality.png',
+        'https://isicons.gumlet.io/logo_wall/JTB-India-Pvt-Ltd.png',
+        'https://isicons.gumlet.io/logo_wall/mach.png'
+      ]
+    },
+    {
+      name: 'Tour and Travel',
+      images: [
+        'https://isicons.gumlet.io/logo_wall/Jagat-Pharma.png',
         'https://isicons.gumlet.io/logo_wall/Kaia-Tourism.png',
-        'https://isicons.gumlet.io/logo_wall/Kaia-Tourism.png',
-        'https://isicons.gumlet.io/logo_wall/Kaia-Tourism.png'
+        'https://isicons.gumlet.io/logo_wall/TRAVLOGUE.png'
       ]
     },
     {
-      name: 'Customer 3',
+      name: 'Online Travel Portel',
       images: [
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png'
+        'https://isicons.gumlet.io/logo_wall/ruptub.png',
+        'https://isicons.gumlet.io/logo_wall/Ease-My-Trip.png',
+        'https://isicons.gumlet.io/logo_wall/Makemytrip.png'
       ]
     },
     {
-      name: 'Customer 4',
+      name: 'Telecommunication',
       images: [
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png'
-      ]
-    },
-    {
-      name: 'Customer 5',
-      images: [
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png'
-      ]
-    },
-    {
-      name: 'Customer 6',
-      images: [
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png',
-        'https://isicons.gumlet.io/logo_wall/Cuemath.png'
+        'https://isicons.gumlet.io/logo_wall/Myoperator.png',
+        'https://isicons.gumlet.io/logo_wall/Exotel.png',
+        'https://isicons.gumlet.io/logo_wall/Spectra-Punj-Llyod-Ltd.png'
       ]
     }
   ];
--- src/app/new-vo/new-vo.component.ts
+++ src/app/new-vo/new-vo.component.ts
@@ -8,11 +8,13 @@ import { MoneyBannerComponent } from "./money-banner/money-banner.component";
 import { ProcessComponent } from "./process/process.component";
 import { UniqueFeaturesComponent } from "./unique-features/unique-features.component";
 import { TrustedReviewsComponent } from "./trusted-reviews/trusted-reviews.component";
+import { ContactFormComponent } from "../forms/contact-form/contact-form.component";
+import { FaqsComponent } from "../home/faqs/faqs.component";
 
 @Component({
   selector: 'app-new-vo',
   standalone: true,
-  imports: [VoHeroComponent, VoOfferingsComponent, StatsComponent, HappyCustomerComponent, WhyChooseUsComponent, MoneyBannerComponent, ProcessComponent, UniqueFeaturesComponent, TrustedReviewsComponent],
+  imports: [VoHeroComponent, VoOfferingsComponent, StatsComponent, HappyCustomerComponent, WhyChooseUsComponent, MoneyBannerComponent, ProcessComponent, UniqueFeaturesComponent, TrustedReviewsComponent, ContactFormComponent, FaqsComponent],
   templateUrl: './new-vo.component.html',
   styleUrl: './new-vo.component.scss'
 })
